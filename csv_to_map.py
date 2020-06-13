04ba8cfcad31dd9ebd45d7e55298c68b18ada4f9
Florens Pauwels <mail@florens.be>
2020-06-13 19:35:57 +0200

    Draw dots

--- csv_to_map.py
+++ csv_to_map.py
@@ -1,10 +1,23 @@
 import csv
-import main
-from PIL import Image
+import calc
+from PIL import Image, ImageDraw
 
-with open('intel.csv') as f:
-    table = csv.reader(f)
+RADIUS = 20  # Radius for dots to be placed
 
-    with Image.open('verdansk-12k2.jpg') as im:
-        for row in table:
-            x, y = main.calc_coord(row[6], row[7])
+
+def main():
+    with open('intel_challenges.csv') as f:
+        table = csv.reader(f)
+
+        with Image.open('stitched_map.png') as im:
+            draw = ImageDraw.Draw(im)
+            for row in table:
+                if row[2] == '1':  # Check if challenge has interactable (coords)
+                    x, y = calc.calc_coord(int(row[5]), int(row[6]))
+                    draw.ellipse((x-RADIUS, y-RADIUS, x+RADIUS, y+RADIUS), 'red')
+
+            im.save('annotated_map.png')
+
+
+if __name__ == '__main__':
+    main()
